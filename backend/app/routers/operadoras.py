46fa44c80dcb345e82251443967e78c967873188
emmillyf <mimig2250@gmail.com>
2025-04-02 15:26:46 -0300

    feat: adicionando HTTPException caso não obtenha resultados nas requisições.

--- backend/app/routers/operadoras.py
+++ backend/app/routers/operadoras.py
@@ -5,17 +5,19 @@ from data.conexao import get_db
 from schemas.schema import OperadorasBase
 import services.service as service
 
-
 router = APIRouter(
 )
 
 @router.get("/operadoras/", response_model=List[OperadorasBase])
 async def listar_operadoras(db: Session = Depends(get_db)):
-    return service.get_all(db)
+    lista_operadoras = service.get_all(db)
+    if not lista_operadoras:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return lista_operadoras
 
 @router.get("/operadoras/{id}", response_model=OperadorasBase)
 async def operadora_by_id(id:int, db: Session = Depends(get_db)):
     operadora_queryset = service.get_by_id(db, id)
-    if operadora_queryset:
-        return operadora_queryset
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if not operadora_queryset:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return operadora_queryset
