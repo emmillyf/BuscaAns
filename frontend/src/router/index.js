2bf76c0105adbbbc4834315d74ec06a1108f7dec
emmillyf <mimig2250@gmail.com>
2025-04-04 20:43:29 -0300

    feat: add docker files.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,10 @@ import ListaOperadoras from '@/views/ListaOperadoras.vue'
 import Home from '@/views/HomeView.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: Home },
+  { path: '/', redirect: '/home' }, 
+  { path: '/home', name: 'Home', component: Home },
   { path: '/operadoras', name: 'ListaOperadoras', component: ListaOperadoras },
-  { path: '/operadoras/buscar/', name: 'Home', component: Home },
+  { path: '/operadoras/buscar/', name: 'BuscarOperadora', component: Home }, 
 ]
 
 const router = createRouter({
